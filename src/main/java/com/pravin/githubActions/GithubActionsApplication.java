6169144a22d68b77084be12f21f7a7a7f05917ca
press <presspravin@outlook.com>
2022-08-07 21:26:07 +0530

    PNO1 : test

---
+++ src/main/java/com/pravin/githubActions/GithubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.pravin.githubActions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsApplication.class, args);
+	}
+
+}
